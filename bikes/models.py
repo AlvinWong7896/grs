45d3910d2d0e0d135b4541066f5147a30ecaf4b8
CK Wong <alvinwong7896@gmail.com>
2023-12-04 20:04:06 +0800

    almost done mockup

--- bikes/models.py
+++ bikes/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Appointment(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    contact_number = models.SmallIntegerField(max_length=8)
-    email_addr = models.EmailField()
+    contact_number = models.SmallIntegerField()
+    email_address = models.EmailField()
 
     def __str__(self):
         return self.first_name + " " + self.last_name
