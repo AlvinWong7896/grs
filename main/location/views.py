11030083eef091110cee5e998855c7f89fdd492d
CK Wong <alvinwong7896@gmail.com>
2024-05-22 12:28:53 +0800

    added user and items. added geocodertimeout and geocoderunavailable for location search

--- main/location/views.py
+++ main/location/views.py
@@ -1,25 +1,47 @@
 from django.shortcuts import render
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 from .models import RepairShop
 from item.models import Item
+import time
 
 
 def index(request):
     latest_items = Item.objects.filter(is_sold=False).order_by("-created_on")[0:10]
     shops = list(RepairShop.objects.values("latitude", "longitude"))
-    # print(shops[:2])
     return render(
         request, "location/index.html", {"shops": shops, "latest_items": latest_items}
     )
+
+
+def get_location(geolocator, address, retries=3, delay=1):
+    for i in range(retries):
+        try:
+            return geolocator.geocode(address + ", Singapore")
+        except (GeocoderTimedOut, GeocoderUnavailable) as e:
+            if i < retries - 1:  # If not the last retry, wait and retry
+                time.sleep(delay)
+            else:  # If the last retry, raise the exception
+                raise e
 
 
 def nearest_shops(request):
     latest_items = Item.objects.filter(is_sold=False).order_by("-created_on")[0:10]
     address = request.GET.get("address")
     geolocator = Nominatim(user_agent="main")
-    location = geolocator.geocode(address + ", Singapore")
-    print(location)
+    try:
+        location = geolocator.geocode(address + ", Singapore")
+    except (GeocoderTimedOut, GeocoderUnavailable):
+        return render(
+            request,
+            "location/index.html",
+            {
+                "error": "Location service is currently unavailable. Please try again later.",
+                "latest_items": latest_items,
+            },
+        )
+
     if location:
         user_location = location.latitude, location.longitude
         nearest_shops = []
@@ -46,12 +68,3 @@ def nearest_shops(request):
         )
     else:
         return render(request, "location/index.html", {"error": "Location not found"})
-
-
-def my_view(request):
-    if not request.user.has_perm("You do not have access to download data"):
-        return render(
-            request,
-            "location/index.html",
-            {"message": "You do not have permission to download data."},
-        )
