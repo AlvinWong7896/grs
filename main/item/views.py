61a1c97a26fedaa4c561f6b561d393e668a08c25
CK Wong <alvinwong7896@gmail.com>
2024-05-24 15:51:59 +0800

    resolved pagination error when searching for items

--- main/item/views.py
+++ main/item/views.py
@@ -10,14 +10,12 @@ from .models import Category, Item
 
 def marketplace(request, category_id=None):
     if request.user.is_authenticated:
-        latest_items = Item.objects.filter(is_sold=False).order_by("-created_on")[0:6]
         items = (
             Item.objects.filter(is_sold=False)
             .exclude(created_by=request.user)
             .order_by("-created_on")
         )
     else:
-        latest_items = Item.objects.filter(is_sold=False).order_by("-created_on")[0:6]
         items = Item.objects.filter(is_sold=False).order_by("-created_on")
 
     categories = Category.objects.all()
@@ -40,16 +38,21 @@ def marketplace(request, category_id=None):
         "item/marketplace.html",
         {
             "categories": categories,
-            "latest_items": latest_items,
             "page": page,
             "items": paginated_items,
         },
     )
 
 
-def search(request):
-    items = Item.objects.filter(is_sold=False)
-    categories = Category.objects.all()
+def search(request, category_id=None):
+    if request.user.is_authenticated:
+        items = (
+            Item.objects.filter(is_sold=False)
+            .exclude(created_by=request.user)
+            .order_by("-created_on")
+        )
+    else:
+        items = Item.objects.filter(is_sold=False).order_by("-created_on")
 
     query = request.GET.get("query")
     if query:
@@ -84,12 +87,35 @@ def search(request):
     if brake_type:
         items = items.filter(brake_type=brake_type)
 
+    categories = Category.objects.all()
+    if category_id is not None:
+        category = get_object_or_404(Category, id=category_id)
+        items = items.filter(category=category)
+
+    paginator = Paginator(items, 20)  # 20 items per page
+    page = request.GET.get("page")
+    try:
+        paginated_items = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page
+        paginated_items = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results
+        paginated_items = paginator.page(paginator.num_pages)
+
+    query_params = request.GET.copy()
+    if "page" in query_params:
+        del query_params["page"]
+    query_string = query_params.urlencode()
+
     return render(
         request,
         "item/marketplace.html",
         {
-            "items": items,
             "categories": categories,
+            "page": page,
+            "items": paginated_items,
+            "query_string": query_string,
         },
     )
 
